42f1186ff0ce7ab790783dadcce8bdf99053d53f
Igor <mr.igor187@gmail.com>
2022-07-24 08:46:16 +0200

    add password validation

--- script.js
+++ script.js
@@ -2,8 +2,52 @@ const testPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{
 
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#passConfirm');
-const emailInput = document.querySelector('.email-input');
+const passwordInput = document.querySelector('.password-input');
 
+const letter = document.getElementById("letter");
+const capital = document.getElementById("capital");
+const number = document.getElementById("number");
+const length = document.getElementById("length");
+const errorBox = document.querySelector(".error-message");
+
+passwordInput.addEventListener('click', () => errorBox.style.display = 'block');
+passwordInput.addEventListener('blur', () => errorBox.style.display = 'none', true);
+
+let lowercase = /[a-z]/g;
+const uppercase = /[A-Z]/g;
+const numbers = /[0-9]/g;
+
+passwordInput.addEventListener('keyup', () => {
+    validatePassword(lowercase, letter);
+    validatePassword(uppercase, capital);
+    validatePassword(numbers, number);
+
+    if(password.value.length >= 8)
+    {
+        length.classList.remove('fa-xmark');
+        length.classList.add('fa-check');
+    }
+    else
+    {
+        length.classList.remove('fa-check');
+        length.classList.add('fa-xmark');
+    }
+});
+
+
+function validatePassword(condition, element)
+{
+    if(password.value.match(condition))
+    {
+        element.classList.remove('fa-xmark');
+        element.classList.add('fa-check');
+    }
+    else
+    {
+        element.classList.remove('fa-check');
+        element.classList.add('fa-xmark');
+    }
+}
 
 //js see if passwords are same
 //if email and number input is good border blue
